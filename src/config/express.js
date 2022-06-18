00ddc8e6f8be9cceb4503607bdc750ae1f993af3
kely123 <kely.piscis.97@gmail.com>
2022-06-17 22:19:13 -0500

    Se agrego el servicio para listar categorias

--- src/config/express.js
+++ src/config/express.js
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import rolesRoutes from "../routes/roles.routes";
 import usersRoutes from "../routes/users.routes";
 import authRoutes from "../routes/auth.routes";
-
+import categoriesRoutes from "../routes/categories.routes";
 const app = express();
 
 app.use(morgan(process.env.NODE_LOG || 'dev'));
@@ -14,5 +14,6 @@ app.use(express.json());
 app.use("/roles", rolesRoutes);
 app.use("/users", usersRoutes);
 app.use("/auth", authRoutes);
+app.use("/categories", categoriesRoutes);
 
 export default app;
