9c7c7274622f4c48903b88d29bb005bc02185e5b
kely123 <kely.piscis.97@gmail.com>
2022-06-23 23:19:07 -0500

    Servicio para obtener carrito de compra del cliente

--- src/controllers/shopping_cart.controller.js
+++ src/controllers/shopping_cart.controller.js
@@ -3,6 +3,7 @@ import { shopping_cart, products } from '../models'
 class ShoppingCartController {
     constructor() {
         this.model = shopping_cart;
+        this.igv = 0.18;
     }
 
     async getAll (req, res) {
@@ -78,5 +79,57 @@ class ShoppingCartController {
             });        
         }
     }
+
+    async getShoppingCartCustomer (req, res) { //obtener carrito de compra del cliente
+        try {
+            //const {id} = 1
+            //const {id} = req.current_user;
+            const records = await this.model.findAll({
+                include: [{
+                    model: products,
+                }],
+                atributes: {
+                    exclude:"user_id",
+                },
+                where: {
+                    user_id: 1,
+                }
+            });
+    
+            if(records)
+            {
+                let total = 0;
+
+                for (let i = 0 ; i < records.length; i++) {
+                    total += records[i].product.price * records[i].quantity
+                }
+
+                let subtotal = total - (total * this.igv)
+                let igv = total * this.igv
+
+        
+                return res.status(200).json({
+                    items:records,
+                    prices:{
+                        'total': total,
+                        'subtotal': subtotal,
+                        'igv': igv,
+                    }
+                });
+            }
+            return res.status(200).json({
+                items:[],
+                prices:{
+                    'total': 0,
+                    'subtotal': 0,
+                    'igv': 0
+                }
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message,
+            });        
+        }
+    }    
 }
 module.exports = ShoppingCartController;
