a08df8e0def3dce9823aec0ba003c6fc30411494
kely123 <kely.piscis.97@gmail.com>
2022-06-27 18:08:33 -0500

    sd

--- src/config/database.js
+++ src/config/database.js
@@ -9,12 +9,15 @@ module.exports = {
         port: process.env.DB_PORT,
         dialect: process.env.DB_CONNECTION
     },
-    producction: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_CONNECTION
+    production: {
+        use_env_variable: "DATABASE_URL",
+        dialect: process.env.DATABASE_CONNECTION,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     }
+    
 }
